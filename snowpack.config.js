f12ed68f4fd5bd19109fce8558a9dc9c7b7df011
vicentefb <chenteferrara@gmail.com>
2021-10-19 14:07:35 -0500

    config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  mount: {
-    // directory name: 'build directory'
-    public: "/",
-    src: "/dist",
-  },
   plugins: ["@snowpack/plugin-dotenv"],
   devOptions: {
     number: process.env.PORT || 3000,
